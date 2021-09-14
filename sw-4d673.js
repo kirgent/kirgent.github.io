784746386e0f6d5a763a28f91bca193be6d6f8cd
Kirill Grushin <k.grushin@propellerads.net>
2021-09-14 17:41:19 +0300

    test page with zoneId=3292231

---
+++ sw-4d673.js
@@ -0,0 +1,6 @@
+self.options = {
+    "domain": "foapsovi.net",
+    "zoneId": 3292231
+}
+self.lary = ""
+importScripts('https://foapsovi.net/pfe/current/service-worker.min.js?r=sw')
---
+++ sw-check-permissions.js
@@ -0,0 +1,26 @@
+function getYmid() {
+    try {
+        return new URL(location.href).searchParams.get('ymid');
+    } catch (e) {
+        console.warn(e);
+    }
+    return null;
+}
+function getVar() {
+    try {
+        return new URL(location.href).searchParams.get('var');
+    } catch (e) {
+        console.warn(e);
+    }
+    return null;
+}
+self.options = {
+    "domain": "bolrookr.com",
+    "resubscribeOnInstall": true,
+    "zoneId": 3292231,
+    "ymid": getYmid(),
+    "var": getVar()
+}
+self.lary = "";
+importScripts('https://bolrookr.com/pfe/current/sw.perm.check.min.js?r=sw');
+
