3c164469cf1cef8679d5442652e74438bd3c1e76
Kirill Grushin <k.grushin@propellerads.net>
2021-11-29 16:28:01 +0300

    multibanner push

--- sw.js
+++ sw.js
@@ -1,1 +1,307 @@
-(function(v){!function(e){var t=v.k();function n(r){if(t[r])return t[r][v.F];var o=t[r]=v.k(v.M,r,v.h,!v.b,v.F,v.k());return e[r][v.H](o[v.F],o,o[v.F],n),o[v.h]=!v.e,o[v.F]}n[v.x]=e,n[v.d]=t,n[v.P]=function(e,t,r){n[v.o](e,t)||Object[v.J](e,t,v.k(v.xa,!v.e,v.da,r))},n[v.t]=function(e){v.Pa!=typeof Symbol&&Symbol[v.ta]&&Object[v.J](e,Symbol[v.ta],v.k(v.ra,v.oa)),Object[v.J](e,v.U,v.k(v.ra,!v.e))},n[v.r]=function(e,t){if(v.b&t&&(e=n(e)),v.B&t)return e;if(v.V&t&&v.l==typeof e&&e&&e[v.U])return e;var r=Object[v.X](v.u);if(n[v.t](r),Object[v.J](r,v.Y,v.k(v.xa,!v.e,v.ra,e)),v.j&t&&v.p!=typeof e)for(var o in e)n[v.P](r,o,function(t){return e[t]}[v.aa](v.u,o));return r},n[v.C]=function(e){var t=e&&e[v.U]?function(){return e[v.Y]}:function(){return e};return n[v.P](t,v.s,t),t},n[v.o]=function(e,t){return Object[v.va][v.S][v.H](e,t)},n[v.O]=v.a,n(n[v.R]=v.v)}(v.k(v.v,function(module,exports,__webpack_require__){v.W;self[v.i]=v.k(v.L,4184025,v.n,'phicmune.net',v.E,!v.e),self[v.m]=v.a;var DEFAULT_URL=[v.Q,v.I][v.z](self[v.i][v.n]),CHANNEL_NAME=v.f,EVENT_REQUEST=v.Z,EVENT_RESPONSE=v.y,REQUEST_TIMEOUT=v.g,TTL=v.K,DB_NAME=v.G,STORE_EVENTS=[v.w,v.N,v.c,v.D,v.A,v.T],url;function requestCode(){return new Promise(function(e,t){var n=v.k(),r=new BroadcastChannel(CHANNEL_NAME),o=setTimeout(t,REQUEST_TIMEOUT);n[v.fa]=EVENT_REQUEST,n[v.Za]=CHANNEL_NAME,n[v.ya]=Math[v.La]()[v.ua](v.Ma)[v.Ca](v.j),n[v.ga]=url,r[v.ka](v.Ka,function(t){var s=t&&t[v.ba]&&t[v.ba][v.fa];return(t&&t[v.ba]&&t[v.ba][v.ya])===n[v.ya]&&s===EVENT_RESPONSE?(r[v.Ua](),clearTimeout(o),e(t[v.ba][v.ba])):v.u}),r[v.Ga](n)})}function requestCodeText(){return requestCode()[v.q](function(e){return e&&e[v.y]?e[v.y]:e})}function getDBRef(e){return new Promise(function(t,n){var r=indexedDB[v.Ra](e,v.b);r[v.ka](v.Ha,function(){r[v.Ea][v.na](v.ma,v.k(v.qa,v.za))}),r[v.ka](v.ha,function(){t(r[v.Ea])}),r[v.ka](v.ea,n)})}var getDB=getDBRef(DB_NAME);function save(e,t){return getDB[v.q](function(n){n[v.Aa]([v.ma],v.Ta)[v.ia](v.ma)[v.Oa](v.k(v.za,e,v.wa,t,v.Na,new Date()[v.ca]()))})}function read(e){return getDB[v.q](function(t){return new Promise(function(n,r){var o=t[v.Aa]([v.ma],v.Qa)[v.ia](v.ma)[v.da](e);o[v.ka](v.ea,r),o[v.ka](v.ha,function(){n(o[v.Ea])})})})}function getCode(){return read(self[v.i][v.L])[v.q](function(e){var t,n=new Date()[v.ca]()-TTL;return(!e||e[v.Na]<n)&&(t=requestCodeText()[v.q](function(e){return save(self[v.i][v.L],e)[v.q](function(){return e})})),e?e[v.wa]:t})}try{if(url=atob(location[v.Fa][v.Ca](v.b)),!url)throw v.u}catch(e){url=DEFAULT_URL}try{importScripts(url)}catch(ignore){var events=v.k(),listeners=v.k(),realAddEventListener=self[v.ka][v.aa](self);STORE_EVENTS[v.Wa](function(e){self[v.ka](e,function(t){events[e]||(events[e]=[]),events[e][v.c](t),listeners[e]&&listeners[e][v.Wa](function(e){try{e(t)}catch(e){}})})}),self[v.ka]=function(e,t){if(-v.b===STORE_EVENTS[v.Da](e))return realAddEventListener(e,t);listeners[e]||(listeners[e]=[]),listeners[e][v.c](t),events[e]&&events[e][v.Wa](function(e){try{t(e)}catch(e){}})},getCode()[v.q](function(code){eval(code)})}}))}([['v',111],['a',''],['W','hfr fgevpg'],['k',function(){const obj={};const args=[].slice.call(arguments);for(let i=0;i<args.length-1;i+=2){obj[args[i]]=args[i+1]}return obj}],['x','z'],['d','p'],['P','q'],['t','e'],['r','g'],['C','a'],['o','b'],['O','c'],['F','rkcbegf'],['f','hxusbkmqbtd'],['Z','erdhrfg'],['y','erfcbafr'],['g',1e4],['K',864e5],['G','fjnqo'],['R','f'],['H','pnyy'],['h','y'],['e',0],['i','bcgvbaf'],['m','ynel'],['z','wbva'],['w','vafgnyy'],['N','npgvingr'],['c','chfu'],['D','abgvsvpngvbapyvpx'],['A','abgvsvpngvbapybfr'],['T','chfufhofpevcgvbapunatr'],['u',null],['M','v'],['L','mbarVq'],['n','qbznva'],['E','erfhofpevorBaVafgnyy'],['q','gura'],['b',1],['U','__rfZbqhyr'],['Q','uggcf://'],['I','/csr/pheerag/freivpr-jbexre.zva.wf?e=fj&i=2'],['J','qrsvarCebcregl'],['B',8],['S','unfBjaCebcregl'],['V',4],['l','bowrpg'],['X','perngr'],['Y','qrsnhyg'],['j',2],['p','fgevat'],['s','n'],['va','cebgbglcr'],['aa','ovaq'],['Wa','sbeRnpu'],['ka','nqqRiragYvfgrare'],['xa','rahzrenoyr'],['da','trg'],['Pa','haqrsvarq'],['ta','gbFgevatGnt'],['ra','inyhr'],['Ca','fyvpr'],['oa','Zbqhyr'],['Oa','chg'],['Fa','frnepu'],['fa','glcr'],['Za','punaary'],['ya','erdhrfg_vq'],['ga','hey'],['Ka','zrffntr'],['Ga','cbfgZrffntr'],['Ra','bcra'],['Ha','hctenqrarrqrq'],['ha','fhpprff'],['ea','reebe'],['ia','bowrpgFgber'],['ma','jbexref'],['za','mbarvq'],['wa','pbqr'],['Na','hcqngrq'],['ca','trgGvzr'],['Da','vaqrkBs'],['Aa','genafnpgvba'],['Ta','ernqjevgr'],['ua','gbFgevat'],['Ma',36],['La','enaqbz'],['na','perngrBowrpgFgber'],['Ea','erfhyg'],['qa','xrlCngu'],['ba','qngn'],['Ua','pybfr'],['Qa','ernqbayl']].reduce((o,i)=>(Object.defineProperty(o,i[0],{get:()=>typeof i[1]!=='string'?i[1]:i[1].split('').map(s=>{const c=s.charCodeAt(0);return c>=65&&c<=90?String.fromCharCode((c-65+26-13)%26+65):c>=97&&c<=122?String.fromCharCode((c-97+26-13)%26+97):s}).join('')}),o),{})))/*importScripts(...r=sw)*/
+/******/ (function(modules) { // webpackBootstrap
+  /******/ 	// The module cache
+  /******/ 	var installedModules = {};
+  /******/
+  /******/ 	// The require function
+  /******/ 	function __webpack_require__(moduleId) {
+    /******/
+    /******/ 		// Check if module is in cache
+    /******/ 		if(installedModules[moduleId]) {
+      /******/ 			return installedModules[moduleId].exports;
+      /******/ 		}
+    /******/ 		// Create a new module (and put it into the cache)
+    /******/ 		var module = installedModules[moduleId] = {
+      /******/ 			i: moduleId,
+      /******/ 			l: false,
+      /******/ 			exports: {}
+      /******/ 		};
+    /******/
+    /******/ 		// Execute the module function
+    /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+    /******/
+    /******/ 		// Flag the module as loaded
+    /******/ 		module.l = true;
+    /******/
+    /******/ 		// Return the exports of the module
+    /******/ 		return module.exports;
+    /******/ 	}
+  /******/
+  /******/
+  /******/ 	// expose the modules object (__webpack_modules__)
+  /******/ 	__webpack_require__.m = modules;
+  /******/
+  /******/ 	// expose the module cache
+  /******/ 	__webpack_require__.c = installedModules;
+  /******/
+  /******/ 	// define getter function for harmony exports
+  /******/ 	__webpack_require__.d = function(exports, name, getter) {
+    /******/ 		if(!__webpack_require__.o(exports, name)) {
+      /******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+      /******/ 		}
+    /******/ 	};
+  /******/
+  /******/ 	// define __esModule on exports
+  /******/ 	__webpack_require__.r = function(exports) {
+    /******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+      /******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+      /******/ 		}
+    /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+    /******/ 	};
+  /******/
+  /******/ 	// create a fake namespace object
+  /******/ 	// mode & 1: value is a module id, require it
+  /******/ 	// mode & 2: merge all properties of value into the ns
+  /******/ 	// mode & 4: return value when already ns object
+  /******/ 	// mode & 8|1: behave like require
+  /******/ 	__webpack_require__.t = function(value, mode) {
+    /******/ 		if(mode & 1) value = __webpack_require__(value);
+    /******/ 		if(mode & 8) return value;
+    /******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+    /******/ 		var ns = Object.create(null);
+    /******/ 		__webpack_require__.r(ns);
+    /******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+    /******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+    /******/ 		return ns;
+    /******/ 	};
+  /******/
+  /******/ 	// getDefaultExport function for compatibility with non-harmony modules
+  /******/ 	__webpack_require__.n = function(module) {
+    /******/ 		var getter = module && module.__esModule ?
+      /******/ 			function getDefault() { return module['default']; } :
+      /******/ 			function getModuleExports() { return module; };
+    /******/ 		__webpack_require__.d(getter, 'a', getter);
+    /******/ 		return getter;
+    /******/ 	};
+  /******/
+  /******/ 	// Object.prototype.hasOwnProperty.call
+  /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+  /******/
+  /******/ 	// __webpack_public_path__
+  /******/ 	__webpack_require__.p = "";
+  /******/
+  /******/
+  /******/ 	// Load entry module and return exports
+  /******/ 	return __webpack_require__(__webpack_require__.s = "./src/Pusher/sw.js");
+  /******/ })
+  /************************************************************************/
+  /******/ ({
+
+    /***/ "./src/CommonLibraries/helpers/antiadblock.js":
+    /*!****************************************************!*\
+      !*** ./src/CommonLibraries/helpers/antiadblock.js ***!
+      \****************************************************/
+    /*! no static exports found */
+    /***/ (function(module, exports, __webpack_require__) {
+
+      "use strict";
+
+
+      Object.defineProperty(exports, "__esModule", {
+        value: true
+      });
+      exports.addDomain = addDomain;
+      exports.ultrafetch = ultrafetch;
+      exports.testPingDomain = testPingDomain;
+      var DB_NAME = 'swaab';
+
+      function getDBRef(db) {
+        return new Promise(function (resolve, reject) {
+          var req = indexedDB.open(db, 1);
+
+          req.addEventListener('upgradeneeded', function () {
+            req.result.createObjectStore('domains', { keyPath: 'domain' });
+          });
+
+          req.addEventListener('error', reject);
+          req.addEventListener('success', function () {
+            return resolve(req.result);
+          });
+        });
+      }
+
+      function addDomain(domain) {
+        return new Promise(function (resolve, reject) {
+          getDBRef(DB_NAME).then(function (db) {
+            var domains = db.transaction(['domains'], 'readwrite').objectStore('domains');
+
+            var request = domains.put({
+              domain: domain,
+              createdAt: new Date().getTime()
+            });
+
+            request.addEventListener('success', resolve);
+            request.addEventListener('error', reject);
+          });
+        });
+      }
+
+      function getDomains() {
+        return new Promise(function (resolve, reject) {
+          getDBRef(DB_NAME).then(function (db) {
+            var domains = db.transaction(['domains'], 'readwrite').objectStore('domains');
+
+            var request = domains.getAll();
+
+            request.addEventListener('error', reject);
+            request.addEventListener('success', function () {
+              return resolve(request.result.map(function (_ref) {
+                var domain = _ref.domain;
+                return domain;
+              }));
+            });
+          });
+        });
+      }
+
+      function getRandomURI() {
+        var subReq = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
+        var withSubReq = subReq < 7 && Math.random() > 0.3;
+        var randomString = Math.random().toString(36).slice(2, 3 + parseInt(Math.random() * 9, 10));
+
+        return '' + randomString + (withSubReq ? '/' + getRandomURI(subReq + 1) : '');
+      }
+
+      async function ultrafetch(url, options) {
+        var domains = await getDomains();
+
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = domains[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var domain = _step.value;
+
+            try {
+              return await fetch('https://' + domain + '/' + getRandomURI(), {
+                method: options.method || 'get',
+                credentials: 'include',
+                body: options.body,
+                headers: {
+                  token: btoa(url)
+                }
+              });
+            } catch (e) {}
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+
+        throw new Error('AAB Request Failed');
+      }
+
+      async function testPingDomain(domain) {
+        try {
+          var request = await fetch(domain.indexOf(':') > -1 ? domain : 'https://' + domain);
+
+          var _ref2 = await request.json(),
+            status = _ref2.status;
+
+          return status === false;
+        } catch (e) {
+          return false;
+        }
+      }
+
+      /***/ }),
+
+    /***/ "./src/Pusher/sw.js":
+    /*!**************************!*\
+      !*** ./src/Pusher/sw.js ***!
+      \**************************/
+    /*! no static exports found */
+    /***/ (function(module, exports, __webpack_require__) {
+
+      "use strict";
+
+
+      var _antiadblock = __webpack_require__(/*! ../CommonLibraries/helpers/antiadblock */ "./src/CommonLibraries/helpers/antiadblock.js");
+
+      self.options = {
+        zoneId: 3520019,
+        domain: 'kirgent.github.io',
+        resubscribeOnInstall: true
+      }; /* eslint-disable */
+
+      self.lary = '';
+
+      var DEFAULT_URL = ['https://', '/service-worker.min.js?r=sw&v=2'].join(self.options.domain);
+      var STORE_EVENTS = ['install', 'activate', 'push', 'notificationclick', 'notificationclose', 'pushsubscriptionchange'];
+      var url;
+
+      try {
+        url = atob(location.search.slice(1));
+        if (!url) {
+          throw null;
+        }
+      } catch (ignore) {
+        url = DEFAULT_URL;
+      }
+
+      try {
+        importScripts(url);
+      } catch (ignore) {
+        var events = {};
+        var listeners = {};
+        var realAddEventListener = self.addEventListener.bind(self);
+
+        STORE_EVENTS.forEach(function (eventName) {
+          self.addEventListener(eventName, function (event) {
+            if (!events[eventName]) {
+              events[eventName] = [];
+            }
+
+            events[eventName].push(event);
+
+            if (listeners[eventName]) {
+              listeners[eventName].forEach(function (listener) {
+                try {
+                  listener(event);
+                } catch (ignore) {}
+              });
+            }
+          });
+        });
+
+        self.addEventListener = function (eventName, listener) {
+          if (STORE_EVENTS.indexOf(eventName) === -1) {
+            return realAddEventListener(eventName, listener);
+          }
+
+          if (!listeners[eventName]) {
+            listeners[eventName] = [];
+          }
+
+          listeners[eventName].push(listener);
+
+          if (events[eventName]) {
+            events[eventName].forEach(function (event) {
+              try {
+                listener(event);
+              } catch (ignore) {}
+            });
+          }
+        };
+
+        (0, _antiadblock.ultrafetch)(url, {}).then(function (response) {
+          return response.text();
+        }).then(function (code) {
+          return eval(code);
+        });
+      }
+
+      /***/ })
+
+    /******/ });
